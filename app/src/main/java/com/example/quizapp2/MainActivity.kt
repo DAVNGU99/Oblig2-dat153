99c6e9b5257aae0c3b523a9e84b19bf531a55c6d
DAVNGU99 <David.h.nguyen1999@gmail.com>
2026-03-11 01:37:21 +0100

    workiung navbar

--- app/src/main/java/com/example/quizapp2/MainActivity.kt
+++ app/src/main/java/com/example/quizapp2/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.quizapp2.ui.theme.QuizApp2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +23,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuizApp2Theme {
-
+                MainScreen()
             }
+        }
+    }
+}
+
+
+//testing navbar
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigation.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Navigation.Home.route) { HomeScreen() }
+            composable(Navigation.Gallery.route) { GalleryScreen() }
+            composable(Navigation.Play.route) { PlayScreen() }
         }
     }
 }
